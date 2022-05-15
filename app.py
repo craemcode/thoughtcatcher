573a403081cb633f34228b9d3accdf3f57169015
Zachariah <ndunguzack13@gmail.com>
2022-05-15 19:21:51 +0300

    Refactored models to different file

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 from flask import Flask, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from model import db,Todo
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+db.init_app(app)
 
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' %self.id
-
+#controller code
 
 @app.route('/', methods = ['POST','GET'])
 def index():
---
+++ model.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+
+
+db = SQLAlchemy()
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' %self.id
