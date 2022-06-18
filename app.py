3b0d8409392004b1725dffdab327dfc742787682
Zachariah <ndunguzack13@gmail.com>
2022-06-18 12:59:43 +0300

    a known user can log in

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from ensurepip import bootstrap
-from flask import Flask, redirect, render_template, request
-from model import db,Todo
+from flask import Flask, redirect, render_template, request, session, flash
+from model import db,Todo,User,Topic,Thought
 from flask_bootstrap import Bootstrap
-from forms import UserForm
+from forms import UserForm, ThoughtForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
@@ -28,14 +28,82 @@ bootstrap = Bootstrap(app)
 #
 #
 @app.route('/', methods = ['POST','GET'])
+
 def index():
+    
     name = None
+    
     form = UserForm()
+    
+    
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
+        #flash("Name successfully submitted")
+        #beat the redirect
         
-    return render_template('index.html', form=form, name=name )
+        user = User.query.filter_by(name=form.name.data).first()
+        
+        if user is None:
+            #session['known'] = False
+            flash('unkown user')
+            session['name'] = form.name.data
+            form.name.data = ''
+            return redirect('/')
+            
+        else:
+            #session['known'] = True
+            session['id'] = user.id
+            session['name'] = form.name.data
+            form.name.data = ''
+            return redirect('/add_thought')
+                 
+    else:
+        pass
+        #fix this my niggar
+
+    
+    
+    return render_template('index.html', 
+                            form1=form,
+                            name=session.get('name'),
+                            #known=session.get('known', False)
+                            )
+
+@app.route('/add_thought',methods=['POST','GET'])
+def add_thought():
+
+    topic = None
+    thought = None
+    form2 = ThoughtForm()    
+    
+    #If new topic, add topic to db, then get id...if old topic just add straight
+    
+    if form2.validate_on_submit():
+       topic = Topic.query.filter_by(name=form2.topic.data).first()
+       if topic is None:
+           topic = Topic(name=form2.topic.data, user_id=session.get['id'])
+           db.session.add(topic)
+           db.session.commit()
+           flash('new topic added')
+
+           thought = Thought(name=form2.thought.data, topic_id=topic.id)
+           db.session.add(thought)
+           db.session.commit()
+           flash('thought successfully added')
+       else:
+            thought = Thought(name=form2.thought.data, topic_id=topic.id)
+            flash('thought successfully added')
+    else:
+        pass
+    
+    
+    return render_template('thoughts.html', 
+                                form2=form2,
+                                name=session.get('name'))                                        
+
+
+
+
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
--- forms.py
+++ forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class UserForm(FlaskForm):
     name = StringField("Please choose 1 name for yourself\n You can use your name",validators=[DataRequired()])
+    submit = SubmitField(validators=[DataRequired()])
+
+
+class ThoughtForm(FlaskForm):
+    topic = StringField("Topic (One word)", validators=[DataRequired()])
+    thought = TextAreaField("Record Your thoughts", validators=[DataRequired()])
     submit = SubmitField(validators=[DataRequired()])
